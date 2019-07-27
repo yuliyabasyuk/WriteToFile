467f1daedbb098a4f78b0909a796e7d3b5dd0862
yuliyabasyuk <yuliya.basyuk.92@gmail.com>
2019-07-27 16:51:09 +0300

    First commit

---
+++ src/test/java/WriteToFileTest.java
@@ -0,0 +1,39 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.*;
+
+
+public class WriteToFileTest {
+    private static final Logger LOG = LoggerFactory.getLogger(WriteToFileTest.class);
+    String logfilePath = "./src/test/logFile.log";
+
+    @Test
+    public void WriteToFileTest() throws Exception{
+        try {
+            BufferedWriter bw =
+                    new BufferedWriter(new FileWriter("./src/test/logFile.log"));
+            bw.write("User doesn't have permissions to access service \n");
+            bw.close();
+        } catch (Exception e){
+            e.getMessage();
+        }
+
+        Assert.assertFalse(permissionsToAccessSkybase(logfilePath));
+    }
+
+    static boolean permissionsToAccessSkybase(String logfilePath) throws IOException {
+        boolean permissionsToAccessSkybase = true;
+        BufferedReader br = new BufferedReader(new FileReader(logfilePath));
+        String sCurrentLine;
+        while ((sCurrentLine = br.readLine()) != null) {
+            if (sCurrentLine.contains("doesn't have permissions to access")) {
+                permissionsToAccessSkybase = false;
+                LOG.info(sCurrentLine);
+            }
+        }
+        return permissionsToAccessSkybase;
+    }
+
+}
